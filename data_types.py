f5e48372537db4737a1de4b49a7cd59380935c77
JemimaNyaguthii <jemimanyaguthiiwambugu@gmail.com>
2023-07-06 07:29:17 +0300

    Update

--- data_types.py
+++ data_types.py
@@ -6,11 +6,16 @@ def chars_separate(word1,word2):
     new_word1=word2[:2] + word1[2:]
     new_word2=word1[:2] + word2[2:]
     result = new_word1 + ' '+ new_word2
-    return result
+    return result  
 print(chars_separate("jem","mima"))
 # 2.Write a Python function that takes a list of 
 # words and returns the longest word and the length of the longest one
 # def longest_word(words):
+# def longest_word(name):
+#     word=max(words,key=len)
+#     return word
+
+# print(longest_word(["cat","apple","eating"]))    
 
 # 3.Write a Python program that accepts a comma-separated
 #  sequence of words as input and prints the distinct words 
@@ -28,7 +33,6 @@ def common_member(list1,list2):
                 output ='True'
                 return output
 print(common_member([21,22,23,1,24],[21,1,3,31,33]))            
-
 
 # 5. Write a Python program to convert a list to a list of dictionaries.
 # Sample lists: ["Black", "Red", "Maroon", "Yellow"], ["#000000", "#FF0000", "#800000", "#FFFF00"]
@@ -61,3 +65,11 @@ print(common)
 divisible_numbers=[r for r in range (1,1000) for d in range(2,9) if r%d==0]
 print(divisible_numbers)
 # 10. Write a Python function to remove all vowels in a string
+
+def remove_vowels():
+    empty=[]
+    vowels=["a","e""i""o""u"]
+    for i in range (len(word)):
+        if word[i] is not vowels:
+            words=empty +word[i]
+
